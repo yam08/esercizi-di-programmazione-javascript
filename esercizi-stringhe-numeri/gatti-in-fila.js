c095723b23a2a2bf858088b87c7a55aa4fed57fe
yam08 <yamir.cepero@gmail.com>
2019-10-09 11:15:58 +0200

    Testing
    Test esercizio gatti-in-fila

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -5,15 +5,24 @@
     b) Il numero dei gatti presenti in ogni fila
   Restituisca in output:
     - Il numero di file risultanti
-    - Nel caso l'ultima fila risulti incompleta indicare il numero di gatti mancanti per completarla (quanti gatti mancano per arrivare a b?)
+    - Nel caso l'ultima fila risulti incompleta indicare il numero
+    di gatti mancanti per completarla (quanti gatti mancano per arrivare a b?)
 
   Esempio:
     Input: numero di gatti = 44, gatti per fila = 6
     Output: numero di file = 8, gatti mancanti = 4
 
   Consigli:
-  Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
+  Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero
+   superiore solo da .5 in su),
   utilizza Math.ceil (arrotonda sempre all'intero superiore).
 
   http://www.imparareaprogrammare.it
 */
+
+var num_Gatti = 51;
+var gatti_per_Fila = 15;
+
+//restituisco il num di file risultante
+var fileRisultante = num_Gatti / gatti_per_Fila;
+console.log(`il numero file risultante è ${fileRisultante}`);
--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -5,6 +5,13 @@
   Esempio:
     Input: a = 1, b = 2, c = 3, d = 4, e = 5
     Output: somma = 15, media = 3
-  
+
   http://www.imparareaprogrammare.it
 */
+
+var a = 3;
+var b = 8;
+var c = 10;
+var d = 25;
+var e = 15;
+console.log(a);

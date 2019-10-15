60e98cfbd51f729cc4aafa96bfe63804d9200207
yam08 <yamir.cepero@gmail.com>
2019-10-15 10:37:42 +0200

    quanti-anni-ha

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -11,3 +11,24 @@
 
   http://www.imparareaprogrammare.it
 */
+var anno_corrente = 2019;
+var anno_di_nascita = 1978;
+var anni_mancanti_a_100 = 100;
+var età;
+
+console.log("anno corrente: " + anno_corrente);
+console.log("anno di nascita: " + anno_di_nascita);
+
+
+//Restituice la età.
+function età(){
+  età = anno_corrente - anno_di_nascita;
+  return 'La tua Età: ' + età;
+}
+//Restituice gli anni mancanti per arrivare a 100.
+function anni_da_raggiungere(){
+  let anni_per_arrivare_a_100 = anni_mancanti_a_100 - età;
+  return "Anni mancanti per arrivare a 100: " + anni_per_arrivare_a_100;
+}
+console.log(età());
+console.log(anni_da_raggiungere());
